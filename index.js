b8eade066ca70327a6ccfa83461d8bfcfe99bb17
Paulina2338 <66403179+Paulina2338@users.noreply.github.com>
2021-05-25 20:50:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.listen(3000,() => console.log('listening at 3000'));
+app.listen(5000,() => console.log('listening at 5000'));
 app.use(express.static('static html'));
